a49ac2578653514190166b896e65ca8c9e6519bd
Teaapoo <danielliu385@gmail.com>
2025-11-30 20:41:59 -0500

    moved all of front-end to the right folder and config

--- frontend+middle/next.config.mjs
+++ frontend+middle/next.config.mjs
@@ -1,4 +1,13 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "upload.wikimedia.org",
+            "images.unsplash.com",
+            "i.imgur.com"
+        ],
+    },
+};
 
 export default nextConfig;
